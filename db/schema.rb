2fcd5148f0cc587e81cd17e0dfa6a5c1527fd76e
Joel Shin <joel@fluxxlabs.com>
2020-01-05 21:43:22 -0800

    add animal count

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200106021054) do
+ActiveRecord::Schema.define(version: 20200106052155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20200106021054) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "count"
   end
 
   create_table "crops", force: :cascade do |t|
